9e32aaba9a7fe564b7ee6f949f0f273450552815
balintl.23d <163391564+Levi968@users.noreply.github.com>
2025-07-23 22:20:30 +0200

    Add files via upload
    Crossout V3 menu animácio weblap

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.getElementById('nav-links');
+const navLinks   = document.getElementById('nav-links');
 
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('show');
